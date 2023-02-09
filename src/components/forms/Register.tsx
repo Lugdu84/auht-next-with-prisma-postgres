0a49e67de002622a3aa6bb0948006b76885f5eb7
David Grammatico <david.lugdunum@gmail.com>
2023-02-09 14:50:57 +0100

    design and validator signup form

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -1,18 +1,121 @@
 import { CiUser } from 'react-icons/ci'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import validator from 'validator'
+import { AiFillLock, AiOutlineMail } from 'react-icons/ai'
+import { BsTelephone } from 'react-icons/bs'
 import Input from '../inputs/input'
 
+const schema = z
+  .object({
+    first_name: z
+      .string()
+      .min(2, 'Le prénom doit contenir au moins 2 caractères')
+      .max(32, 'Le prénom doit contenir au plus 32 caractères')
+      .regex(/^[a-zA-ZÀ-ÿ]+$/, 'Le prénom ne peut contenir que des lettres'),
+    last_name: z
+      .string()
+      .min(2, 'Le nom doit contenir au moins 2 caractères')
+      .max(32, 'Le nom doit contenir au plus 32 caractères')
+      .regex(/^[a-zA-ZÀ-ÿ]+$/, 'Le nom ne peut contenir que des lettres'),
+    email: z.string().email("L'email doit être valide"),
+    phone: z
+      .string()
+      .regex(
+        /^0[67]([-. ]?[0-9]{2}){4}$/,
+        'Le numéro de téléphone est invalide'
+      ),
+    password: z
+      .string()
+      .min(6, 'Le mot de passe doit contenir au moins 8 caractères'),
+    confirm_password: z.string(),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: 'Les mots de passe ne correspondent pas',
+    path: ['confirm_password'],
+  })
+
+type FormSchemaType = z.infer<typeof schema>
+
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(schema),
+  })
+
+  console.log(watch())
+
+  const onSubmit = (data) => console.log(data)
   return (
-    <form className="my-8 text-sm">
+    <form className="my-8 text-sm" onSubmit={handleSubmit(onSubmit)}>
       <div className="gap-2 md:flex">
         <Input
           name="first_name"
-          label="First name"
+          label="Prénom"
           type="text"
           Icon={CiUser}
-          placeholder="exemple"
+          placeholder="David"
+          register={register}
+          error={errors?.first_name?.message}
+          disabled={isSubmitting}
+        />
+        <Input
+          name="last_name"
+          label="Nom"
+          type="text"
+          Icon={CiUser}
+          placeholder="Durand"
+          register={register}
+          error={errors?.last_name?.message}
+          disabled={isSubmitting}
+        />
+        <Input
+          name="email"
+          label="Email"
+          type="text"
+          Icon={AiOutlineMail}
+          placeholder="monadresse@adresse.com"
+          register={register}
+          error={errors?.email?.message}
+          disabled={isSubmitting}
+        />
+        <Input
+          name="phone"
+          label="Numéro de téléphone"
+          type="number"
+          Icon={BsTelephone}
+          placeholder="06-34-41-89-03"
+          register={register}
+          error={errors?.phone?.message}
+          disabled={isSubmitting}
+        />
+        <Input
+          name="password"
+          label="Mot de passe"
+          type="password"
+          Icon={AiFillLock}
+          placeholder="Votre mot de passe"
+          register={register}
+          error={errors?.password?.message}
+          disabled={isSubmitting}
+        />
+        <Input
+          name="confirm_password"
+          label="Confirmer votre mot de passe"
+          type="password"
+          Icon={AiFillLock}
+          placeholder="Votre mot de passe"
+          register={register}
+          error={errors?.confirm_password?.message}
+          disabled={isSubmitting}
         />
       </div>
+      <button type="submit">Créer</button>
     </form>
   )
 }
--- src/components/inputs/input.tsx
+++ src/components/inputs/input.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IconBaseProps } from 'react-icons'
+import { IoAlertCircle } from 'react-icons/io5'
 
 interface InputProps {
   type: string
@@ -7,21 +8,46 @@ interface InputProps {
   label: string
   placeholder: string
   Icon: React.FC<IconBaseProps>
+  register: any
+  error: any
+  disabled: boolean
 }
 
-function Input({ type, name, label, placeholder, Icon }: InputProps) {
+function Input(props: InputProps) {
+  const { type, name, label, placeholder, Icon, register, error, disabled } =
+    props
   return (
-    <div className="relative mt-1 rounded-md shadow-sm">
-      <div className="pointer-event-none absolute left-0 inset-y-0 flex items-center pl-3">
-        <span className="text-gray-500 text-sm">
-          <Icon />
-        </span>
+    <div className="mt-3">
+      <label htmlFor={name} className=" text-gray-700">
+        {label}
+      </label>
+      <div className="relative mt-1 rounded-md">
+        <div
+          className="pointer-event-none absolute left-0 inset-y-0 flex items-center pl-3"
+          style={{ transform: `${error ? 'translateY(-12px)' : ''}` }}
+        >
+          <span className="text-gray-500 text-sm">
+            <Icon />
+          </span>
+        </div>
+        <input
+          type={type}
+          placeholder={placeholder}
+          className="w-full py-2 pr-7 pl-8 block rounded-md border border-gray-600  outline-offset-5 outline-gray-800 focus:outline-blue-600 text-sm focus:border-transparent"
+          {...register(name)}
+          disabled={disabled}
+          style={{ borderColor: error ? '#ED4337' : '#4F46E5' }}
+        />
+        {error && (
+          <div className="fill-red-500 ">
+            <IoAlertCircle
+              fill="#ED4337"
+              className="absolute right-2 top-2.5 text-xl"
+            />
+            <p className="text-sm text-[#ED4337] mt-1">{error}</p>
+          </div>
+        )}
       </div>
-      <input
-        type={type}
-        placeholder={placeholder}
-        className="w-full py-2 pr-7 pl-8 block rounded-md border border-gray-600  outline-offset-5 outline-gray-800 focus:outline-blue-600 text-sm focus:border-transparent"
-      />
     </div>
   )
 }
